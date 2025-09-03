91f14afec691e2f882708c620ec914cc6be35a38
Ram Srikanth <162788469+ram-srikanth@users.noreply.github.com>
2025-09-03 10:53:46 +0530

    Create left_factoring.c

---
+++ left_factoring.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+    char gram[20], part1[20], part2[20], modifiedGram[20], newGram[20];
+    int i, j=0, k=0, pos;
+
+    printf("Enter Production : A->");
+    fgets(gram, sizeof(gram), stdin);
+    gram[strcspn(gram, "\n")] = '\0'; // remove newline
+
+    for(i=0; gram[i]!='|' && gram[i]!='\0'; i++, j++)
+        part1[j] = gram[i];
+    part1[j] = '\0';
+
+    for(j=++i, i=0; gram[j]!='\0'; j++, i++)
+        part2[i] = gram[j];
+    part2[i] = '\0';
+
+    for(i=0; i<strlen(part1) && i<strlen(part2); i++) {
+        if(part1[i] == part2[i]) {
+            modifiedGram[k++] = part1[i];
+            pos = i + 1;
+        }
+    }
+
+    modifiedGram[k++] = 'X';
+    modifiedGram[k] = '\0';
+
+    for(i=pos, j=0; part1[i]!='\0'; i++, j++)
+        newGram[j] = part1[i];
+    newGram[j++] = '|';
+
+    for(i=pos; part2[i]!='\0'; i++, j++)
+        newGram[j] = part2[i];
+    newGram[j] = '\0';
+
+    printf("\nA->%s", modifiedGram);
+    printf("\nX->%s\n", newGram);
+}
+
